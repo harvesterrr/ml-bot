15c6a9bb230f0bc2f8221a44b9ade1420385e034
harvesterrr <gabrielmorck@outlook.com>
2020-12-02 16:13:40 +0100

    Update training.py

--- ml-bot/training.py
+++ ml-bot/training.py
@@ -7,9 +7,9 @@ import tqdm
 from nltk.stem.lancaster import LancasterStemmer
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import optim
 from torch.utils.data import Dataset, DataLoader 
+from net import Net
 
 # model save path
 path = "model.pth"
@@ -71,20 +71,6 @@ class dataset(Dataset):
 
     def __len__(self):
         return self.samples
-
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(len(arr), 32) # input layer
-        self.fc2 = nn.Linear(32, 32) # hidden layer
-        self.fc3 = nn.Linear(32, len(tags)) # output layer
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-
-        return x
 
 if __name__ == '__main__':
     if torch.cuda.is_available() == True:
